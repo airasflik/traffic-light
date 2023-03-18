186947d1e032b3866c90e73aca2aabd2844cf9a0
Nikita <airasflik@gmail.com>
2023-03-18 18:42:20 +0700

    Initial Commit

---
+++ traffic light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traffic light
+//
+//  Created by Nikita on 18.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
