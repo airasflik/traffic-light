3c0ef79ec0b7e48bd589b15f7f7daef1a6af4b70
Nikita <airasflik@gmail.com>
2023-03-18 21:02:46 +0700

    Connect storyboard with ViewController and implemented traffic light logic

--- traffic light/ViewController.swift
+++ traffic light/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        redLight.layer.cornerRadius = 75
+        yellowLight.layer.cornerRadius = 75
+        greenLight.layer.cornerRadius = 75
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        }
+    
+    @IBAction func startButtonTapped() {
+        let alpha: CGFloat = 1
+        
+        switch alpha {
+        case redLight.alpha:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        case yellowLight.alpha:
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        case greenLight.alpha:
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        default:
+            redLight.alpha = 1
+            startButton.setTitle("NEXT", for: .normal)
+        }
     }
-
-
+    
 }
 
